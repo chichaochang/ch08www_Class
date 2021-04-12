6080afaf241cd4f58b75234da73e3fda183cca0e
chichaochang <terence@mail.cjcu.edu.tw>
2021-04-12 15:02:09 +0800

    up to 8.3.1

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from mysite import models
+from mysite import models, forms
 
 
 # Create your views here.
@@ -35,3 +35,52 @@ def index(request, pid=None, del_pass=None):
         message = '成功儲存! 請記得你的編輯密碼[{}]!'.format(user_pass)
 
     return render(request, 'index.html', locals())
+
+
+def listing(request):
+    posts = models.Post.objects.all().order_by('-pub_time')[:150]
+    moods = models.Mood.objects.all()
+    return render(request, 'listing.html', locals())
+
+
+def posting(request):
+    moods = models.Mood.objects.all()
+    try:
+        user_id = request.POST['user_id']
+        user_pass = request.POST['user_pass']
+        user_post = request.POST['user_post']
+        user_mood = request.POST['mood']
+    except:
+        user_id = None
+        message = '如果要張貼訊息，則每一個欄位都要填寫'
+    if user_id is not None:
+        mood = models.Mood.objects.get(status=user_mood)
+        post = models.Post.objects.create(mood=mood, nickname=user_id, del_pass=user_pass, message=user_post)
+        post.save()
+        message = '成功儲存! 請記得你的編輯密碼[{}]!'.format(user_pass)
+
+    return render(request, 'posting.html', locals())
+
+
+def contact(request):
+    if request.method == 'POST':
+        form = forms.ContactForm(request.POST)
+        if form.is_valid():
+            message = "感謝您的來信"
+            user_name = form.cleaned_data['user_name']
+            user_city = form.cleaned_data['user_city']
+            user_school = form.cleaned_data['user_school']
+            user_email  = form.cleaned_data['user_email']
+            user_message = form.cleaned_data['user_message']
+        else:
+            message = "請檢查您輸入的資訊是否正確"
+    else:
+        form = forms.ContactForm()
+    return render(request, 'contact.html', locals())
+
+
+def post2db(request):
+    post_form = forms.PostForm()
+    moods = models.Mood.objects.all()
+    message = '如果要張貼訊息，則每一個欄位都要填寫....'
+    return render(request, 'post2db.html', locals())
